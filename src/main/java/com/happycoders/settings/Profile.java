ab653aafe87a912d3a0d5cbf5b74346e69d3638d
김민재 <joshuara7235@gmail.com>
2021-11-10 00:37:10 +0900

    profile setting front tier / refactor for fragement -> setting menu bar

---
+++ src/main/java/com/happycoders/settings/Profile.java
@@ -0,0 +1,24 @@
+package com.happycoders.settings;
+
+import com.happycoders.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    public Profile(Account account) {
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+    }
+
+}
---
+++ src/main/java/com/happycoders/settings/SettingsController.java
@@ -0,0 +1,19 @@
+package com.happycoders.settings;
+
+import com.happycoders.account.CurrentUser;
+import com.happycoders.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SettingsController {
+
+    @GetMapping("/settings/profile")
+    public String profileUpdateForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new Profile(account));
+        return "settings/profile";
+    }
+
+}
