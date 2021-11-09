d97c44db134f8e884a8fd2aee50af4b9ba622460
김민재 <joshuara7235@gmail.com>
2021-11-09 18:14:55 +0900

    RememberMe configuration for stay login session info (remember me session)

--- src/main/java/com/happycoders/config/SecurityConfig.java
+++ src/main/java/com/happycoders/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.happycoders.config;
 
+import com.happycoders.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity // 내가 이제 시큐리티 설정을 하겠다!
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //UserDetails를 구현한 accountService 객체를 불러와준다. -> rememberme 에서 사용하기 위함
+     private final AccountService accountService;
+
+     private final DataSource dataSource;
 
     //WebSecurity Configurer Adapter에서 overriding하면된다.
     @Override
@@ -27,6 +40,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout()
                 .logoutSuccessUrl("/"); //logout 성공시 home으로 보내기
 
+        //cookie를 통해 로그인 유지 및 보안
+        http.rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository());
+
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository () {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Override
