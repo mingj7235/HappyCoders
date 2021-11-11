02795c5c38a371fd30ada2d78856e4ce4fe6fdb1
김민재 <joshuara7235@gmail.com>
2021-11-11 23:28:49 +0900

    Spring security -> security context test setting (create annotation, Before Annotation)

---
+++ src/test/java/com/happycoders/WithAccountSecurityContextFactory.java
@@ -0,0 +1,35 @@
+package com.happycoders;
+
+import com.happycoders.account.AccountService;
+import com.happycoders.account.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@RequiredArgsConstructor
+public class WithAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("minjae");
+        signUpForm.setEmail("3mins1@naver.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+
+        UserDetails principal = accountService.loadUserByUsername(nickname);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        return context;
+    }
+
+}
--- src/test/java/com/happycoders/settings/SettingsControllerTest.java
+++ src/test/java/com/happycoders/settings/SettingsControllerTest.java
@@ -1,18 +1,22 @@
 package com.happycoders.settings;
 
+import com.happycoders.WithAccount;
 import com.happycoders.account.AccountRepository;
 import com.happycoders.account.AccountService;
 import com.happycoders.account.SignUpForm;
 import com.happycoders.domain.Account;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,24 +35,26 @@ class SettingsControllerTest {
     AccountRepository accountRepository;
 
     @BeforeEach
-    void beforeEach () {
-        SignUpForm signUpForm = new SignUpForm();
-        signUpForm.setNickname("minjae");
-        signUpForm.setEmail("3mins1@naver.com");
-        signUpForm.setPassword("12345678");
-        accountService.processNewAccount(signUpForm);
+    void beforeEach() {
+
     }
 
-    @
+    @AfterEach
+    void afterEach () {
+        accountRepository.deleteAll();
+    }
+
+//    @WithUserDetails(value = "minjae", setupBefore = TestExecutionEvent.TEST_EXECUTION) //@BeforeEach 전에 실행이 되는 bug가 있다.
+    @WithAccount(value = "minjae") //test를 위해서 만든 annotation
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
     void updateProfile() throws Exception {
         String bio = "짧은 소개를 수정하는 경우";
 
         mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
-                .param("bio", bio)
+                        .param("bio", bio)
                         .with(csrf())
-        )
+                )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
                 .andExpect(flash().attributeExists("message"));
@@ -56,4 +62,5 @@ class SettingsControllerTest {
         Account account = accountRepository.findByNickname("minjae");
         assertEquals(bio, account.getBio());
     }
+
 }
