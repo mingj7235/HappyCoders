e464ee8361bcf8fe2779037127dbd455d9ad0331
김민재 <joshuara7235@gmail.com>
2021-11-12 14:30:38 +0900

    pom.xml formatting, profile validation, Test code completed

--- src/main/java/com/happycoders/settings/Profile.java
+++ src/main/java/com/happycoders/settings/Profile.java
@@ -3,17 +3,22 @@ package com.happycoders.settings;
 import com.happycoders.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor //기본생성자가 없으면 만들어진 생성자로 접근을 하는데, 파라미터 값을 찾을 수 없을 경우 NPE발생
 public class Profile {
 
+    @Length (max = 35)
     private String bio;
 
+    @Length (max = 50)
     private String url;
 
+    @Length(max = 50)
     private String occupation;
 
+    @Length (max = 50)
     private String location;
 
     public Profile(Account account) {
--- src/test/java/com/happycoders/settings/SettingsControllerTest.java
+++ src/test/java/com/happycoders/settings/SettingsControllerTest.java
@@ -3,7 +3,6 @@ package com.happycoders.settings;
 import com.happycoders.WithAccount;
 import com.happycoders.account.AccountRepository;
 import com.happycoders.account.AccountService;
-import com.happycoders.account.SignUpForm;
 import com.happycoders.domain.Account;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,12 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -40,11 +39,11 @@ class SettingsControllerTest {
     }
 
     @AfterEach
-    void afterEach () {
+    void afterEach() {
         accountRepository.deleteAll();
     }
 
-//    @WithUserDetails(value = "minjae", setupBefore = TestExecutionEvent.TEST_EXECUTION) //@BeforeEach 전에 실행이 되는 bug가 있다.
+    //    @WithUserDetails(value = "minjae", setupBefore = TestExecutionEvent.TEST_EXECUTION) //@BeforeEach 전에 실행이 되는 bug가 있다.
     @WithAccount(value = "minjae") //test를 위해서 만든 annotation
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
@@ -61,6 +60,39 @@ class SettingsControllerTest {
 
         Account account = accountRepository.findByNickname("minjae");
         assertEquals(bio, account.getBio());
+    }
+
+    @WithAccount(value = "minjae") //test를 위해서 만든 annotation
+    @DisplayName("프로필 수정하기 - 입력값 에러")
+    @Test
+    void updateProfile_error() throws Exception {
+        String bio = "길게 소개를 수정하는 경우 (35자이상 일시 오류가난다.) 길게 소개를 수정하는 경우 (35자이상 일시 오류가난다.) 길게 소개를 수정하는 경우 (35자이상 일시 오류가난다.) 길게 소개를 수정하는 경우 (35자이상 일시 오류가난다.)";
+
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio)
+                        .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account account = accountRepository.findByNickname("minjae");
+        assertNull(account.getBio());
+    }
+
+    @WithAccount(value = "minjae") // 인증정보를 제공하기 위해서 !! 이게 있어야 한다.
+    @DisplayName("프로필 수정폼")
+    @Test
+    void updateProfileForm() throws Exception {
+        String bio = "짧은 소개 요청 ";
+
+        mockMvc.perform(get(SettingsController.SETTINGS_PROFILE_URL)
+                )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"));
     }
 
 }
