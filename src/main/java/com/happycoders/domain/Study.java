6dd0dcf3a1a4a821bbfb21cc924437978fa3f172
김민재 <joshuara7235@gmail.com>
2021-11-24 23:16:22 +0900

    Study entity modeling

---
+++ src/main/java/com/happycoders/domain/Study.java
@@ -0,0 +1,63 @@
+package com.happycoders.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Study {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers;
+
+    @ManyToMany
+    private Set<Account> members;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER) // Lob 값이지만, 무조건 가져오게 하기 위해 eager로
+    private String fullDescription;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags;
+
+    @ManyToMany
+    private Set<Zone> zones;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+}
