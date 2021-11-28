20774ffd888309718d3b7f7fcb5bf83e2426c815
김민재 <joshuara7235@gmail.com>
2021-11-28 22:17:25 +0900

    NamedEntityGraph annotation -> eager fetch

--- src/main/java/com/happycoders/study/StudyRepository.java
+++ src/main/java/com/happycoders/study/StudyRepository.java
@@ -1,6 +1,7 @@
 package com.happycoders.study;
 
 import com.happycoders.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +10,7 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
 
     boolean existsByPath(String path);
 
+    @EntityGraph (value = "Study.withAll", type = EntityGraph.EntityGraphType.LOAD)
     Study findByPath(String path);
 
 
