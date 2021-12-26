3a349e185d2aa362ba1c24504d0da027650b476d
김민재 <joshuara7235@gmail.com>
2021-12-26 23:29:39 +0900

    simple test code for create study

--- src/test/java/com/happycoders/study/StudyControllerTest.java
+++ src/test/java/com/happycoders/study/StudyControllerTest.java
@@ -1,7 +1,61 @@
 package com.happycoders.study;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.happycoders.WithAccount;
+import com.happycoders.account.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
 class StudyControllerTest {
 
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected StudyService studyService;
+
+    @Autowired
+    protected StudyRepository studyRepository;
+
+    @Autowired
+    protected AccountRepository accountRepository;
+
+    @Test
+    @WithAccount("minjae")
+    @DisplayName("스터디 개설 폼 조회")
+    void createStudyForm () throws Exception {
+        mockMvc.perform(get("/new-study"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("study/form"))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("studyForm"));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
