20b74ab1d748f600209c8ab921c199bbfb1e23ac
김민재 <joshuara7235@gmail.com>
2021-12-27 23:07:31 +0900

    Test code for create study success

--- src/test/java/com/happycoders/study/StudyControllerTest.java
+++ src/test/java/com/happycoders/study/StudyControllerTest.java
@@ -2,6 +2,8 @@ package com.happycoders.study;
 
 import com.happycoders.WithAccount;
 import com.happycoders.account.AccountRepository;
+import com.happycoders.domain.Account;
+import com.happycoders.domain.Study;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Transactional
@@ -41,6 +47,25 @@ class StudyControllerTest {
                 .andExpect(view().name("study/form"))
                 .andExpect(model().attributeExists("account"))
                 .andExpect(model().attributeExists("studyForm"));
+    }
+
+    @Test
+    @WithAccount("minjae")
+    @DisplayName("스터디 개설 - 완료")
+    void createStudy_success () throws Exception {
+        mockMvc.perform(post("/new-study")
+                .param("path", "test-path")
+                .param("title", "study title")
+                .param("shortDescription", "short description of a study")
+                .param("fullDescription", "full description of a study")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/test-path"));
+
+        Study study = studyRepository.findByPath("test-path");
+        assertNotNull(study);
+        Account account = accountRepository.findByNickname("keesun");
+        assertTrue(study.getManagers().contains(account));
     }
 }
 
